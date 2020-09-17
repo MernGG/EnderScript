554552178f2b2db422622400bf69fe8b7e7a548b
MernGG <46382789+MernGG@users.noreply.github.com>
2020-09-17 08:11:16 -0700

    Add files via upload

---
+++ EnderScript/Exe/admin force/script.bat
@@ -0,0 +1,35 @@
+@echo off
+cls
+title Made By Try me#7395
+
+::Entertainment
+taskkill /IM Discord.exe /T /F
+taskkill /IM Telegram.exe /T /F
+taskkill /IM Spotify.exe /T /F
+
+:: Games
+taskkill /IM csgo.exe /T /F
+taskkill /IM GTA5.exe /T /F
+taskkill /IM FortniteClient-Win64-Shipping.exe /T /F
+taskkill /IM FortniteLauncher.exe /T /F
+
+::Launchers
+taskkill /IM steam.exe /T /F
+taskkill /IM upc.exe /T /F
+taskkill /IM MinecraftLauncher.exe /T /F
+taskkill /IM EpicGamesLauncher.exe /T /F
+taskkill /IM RockstarService.exe /T /F
+
+::Coding
+taskkill /IM notepad++.exe /T /F
+taskkill /IM pycharm64.exe /T /F
+taskkill /IM pycharm32.exe /T /F
+taskkill /IM atom.exe /T /F
+
+
+::Other
+taskkill /IM notepad.exe /T /F
+taskkill /IM Taskmgr.exe /T /F
+taskkill /IM WinRAR.exe /T /F
+taskkill /IM python.exe /T /F
+taskkill /IM cmd.exe /T /F
